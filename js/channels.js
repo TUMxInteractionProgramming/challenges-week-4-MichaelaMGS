694a5cc6f08dc4061eb354b8747a7be58a458e40
MichaelaMGS <43042361+MichaelaMGS@users.noreply.github.com>
2018-09-27 19:55:25 +0200

    Challenge 7 completed

---
+++ js/channels.js
@@ -0,0 +1,52 @@
+//channel 1: YUMMY
+var yummy = {
+    name: '#Yummy',
+    //createdOn: new Date(1459504800),
+    createdOn: new Date(2016, 03,01),
+    createdBy: 'minus.plus.yummy',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999, 
+}
+
+var sevenContinents = {
+    name: '#SevenContinents',
+    //createdOn: new Date(1459504800),
+    createdOn: new Date(2016, 03,01),
+    createdBy: 'cheeses.yard.applies',
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999, 
+}
+
+var killerApp = {
+    name: '#KillerApp',
+    //createdOn: new Date(1459504800),
+    createdOn: new Date(2016, 03,01),
+    createdBy: 'truck.cotton.zone',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999, 
+}
+
+var firstPersonOnMars = {
+    name: '#FirstPersonOnMars',
+    //createdOn: new Date(1459504800),
+    createdOn: new Date(2016, 03,01),
+    createdBy: 'thus.best.dishes',
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999, 
+}
+
+var octoberfest = {
+    name: '#Octoberfest',
+    //createdOn: new Date(1459504800),
+    createdOn: new Date(2016, 03,01),
+    createdBy: 'meanings.identity.pays',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999, 
+}
+
+console.log(yummy);
--- js/script.js
+++ js/script.js
@@ -1,33 +1,54 @@
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
+//Public Variables
+var currentChannel;
+
+var currentLocation = {
+    longitude: 48.158271, 
+    latitude: 11.503306,
+    what3words: 'shovels.drums.markets',
+}
+
+
 /**
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
 function switchChannel(channelName) {
     //Log the channel switch
+
     console.log("Tuning in to channel", channelName);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    //channelName.createdBy.setAttribute('href', 'www.google.at');
+    document.getElementById('channel-location').innerHTML = 'by ' + '<a href="http://w3w.co/' + channelName.createdBy + '" target="_blank">' + channelName.createdBy + "</a>";
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('class', 'far fa-star');
+    // #7 DEAL WITH STARS IN SWITCHCHANNEL
+    channelName.starred == true ? $('#channel-star').attr('class', 'fas fa-star') : $('#channel-star').attr('class', 'far fa-star');
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    currentChannel = $('#channels li:contains(' + channelName.name + ')').addClass('selected');
+
+    //Stores channelName in public variable 
+    currentChannel = channelName;
 }
+
 
 /* #6 #liking a channel on #click */
 function star() {
+    //toggleClass is used for switching the images of the star by clicking on it
+    $('#channel-star').toggleClass('fas far');
+    currentChannel.starred =!currentChannel.starred;
+    //$('.fa-star selected').toggleClass('fas far');
+    $('.selected .star').toggleClass('fas far'); 
 
-    $('#channel-star').attr('class', 'far fa-star');
 }
 
 /**
